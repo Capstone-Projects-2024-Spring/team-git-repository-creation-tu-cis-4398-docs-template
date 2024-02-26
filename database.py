f3cd4ee8aa51141e6fc5ece5299265c3ac7e1ef6
Eric Reizas <tuo17224@temple.edu>
2024-02-25 20:11:11 -0500

    Deleted unnecessary files

--- database.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-from app import App
-STR_MAX_SIZE = 65535
-class Database:
-    def __init__(self,app:App):
-        self._app = app
