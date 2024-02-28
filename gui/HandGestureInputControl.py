22cafacb290ba625a16133794bf7644311f43fe4
Zaina <tun35209@temple.edu>
2024-02-28 17:01:43 -0500

    all classes + methods for gui block

---
+++ gui/HandGestureInputControl.py
@@ -0,0 +1,12 @@
+import pygame
+
+class HandGestureInputControl:
+    def __init__(self):
+        self.gesture = "up"
+
+    def captureGesture(void):
+        return 
+
+    def mapGestureToAction(gesture:str):
+        return
+ 
---
+++ gui/MiniGameSelectionScreen.py
@@ -0,0 +1,17 @@
+import pygame
+
+class MiniGameSelectionScreen:
+    def __init__(self,menuOptions:str[3]):
+        self.menuOptions = menuOptions
+
+    def displayGames(void):
+        return
+
+    def handleSelection(selection:str):        
+        return 
+    
+    def startSelectedGame(game:str):
+        return 
+    
+    def exitGame():
+        return
