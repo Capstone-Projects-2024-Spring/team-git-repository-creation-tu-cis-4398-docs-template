d443b155f39b53ca6f65c1f88e30584dd1a94a0d
JasonC136 <112498586+JasonC136@users.noreply.github.com>
2024-02-28 21:28:33 +0000

    Added Docu for Dyn_Text, Leaderboard, and Keyboard_Animation

---
+++ Dynamic_Text_Gen.js
@@ -0,0 +1,18 @@
+class DynamicTextGen {
+    /**
+     * Creates an instance of DynamicTextGen.
+     * @constructor
+     * @param {string[]} textPatterns - An array of predefined text patterns.
+     */
+    constructor(textPatterns) {
+        this.textPatterns = textPatterns;
+    }
+  
+    /**
+     * Generates dynamic text based on predefined patterns.
+     * @returns {string} The generated dynamic text.
+     */
+    generateText() {
+        // Implementation for generating dynamic text
+    }
+  }
