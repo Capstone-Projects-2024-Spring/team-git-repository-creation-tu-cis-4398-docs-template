73d13500934cc31527d2bea453c791cf0e6a2aed
Bryan Reiter <tul13138@temple.edu>
2024-02-29 17:28:01 -0500

    added a few more tests just for validation sake

--- intelligest_smart_home/tests/proof_of_tests.py
+++ intelligest_smart_home/tests/proof_of_tests.py
@@ -5,6 +5,24 @@ class TestAddNumbers(unittest.TestCase):
     def test_addition(self):
         result = add_numbers(1, 2)
         self.assertEqual(result, 3)
+    
+    def test_addition_negative(self):
+        result = add_numbers(-1, -2)
+        self.assertEqual(result, -3)
+
+    def test_addition_zero(self):
+        result = add_numbers(0, 0)
+        self.assertEqual(result, 0)
+
+    def test_addition_float(self):
+        result = add_numbers(1.5, 2.5)
+        self.assertEqual(result, 4)
+    
+    def test_addition_positive_negative(self):
+        result = add_numbers(1, -2)
+        self.assertEqual(result, -1)    
+
 
 if __name__ == '__main__':
     unittest.main()
+
