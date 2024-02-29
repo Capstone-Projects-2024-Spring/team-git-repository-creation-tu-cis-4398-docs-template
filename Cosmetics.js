a1d32526db8a5dac38c664799fbeef0f971ad142
Wenjie Chen <tun71931@temple.edu>
2024-02-29 10:42:58 -0500

    indentation for cosmetics file

--- Cosmetics.js
+++ Cosmetics.js
@@ -3,49 +3,49 @@
  * including functions of storing changing the cosmetics theme, cosmetics points, and spending cosmetics points 
  * @class
  */
-class Cosmetics{
-/**
- * create an instance of cosmetics
- * @constructor
- * @param {String []} availableThemes Showing the array of available custometic themes for purchase
- */
-constructor(availableThemes){
+class Cosmetics {
+    /**
+     * create an instance of cosmetics
+     * @constructor
+     * @param {String []} availableThemes Showing the array of available custometic themes for purchase
+     */
+    constructor(availableThemes) {
 
-    this.availableThemes = availableThemes;
+        this.availableThemes = availableThemes;
 
-}
+    }
 
-/**
- * changes the theme of the cosmetics to the desired choice
- * @param {String} theme - an input of selected theme
- * @returns {void} 
- */
-changeTheme(theme){
-    //implements changed theme for cosmetics
-    //takes in one of the available theme of cosmetics
-    //validates whether the theme exists or not
-}
+    /**
+     * changes the theme of the cosmetics to the desired choice
+     * @param {String} theme - an input of selected theme
+     * @returns {void} 
+     */
+    changeTheme(theme) {
+        //implements changed theme for cosmetics
+        //takes in one of the available theme of cosmetics
+        //validates whether the theme exists or not
+    }
 
-/**
- * a function for player earning points recorded into the user profile
- * @param {Number} earnedpoints - takes in a number of gained points
- * @returns {Number} Number - returns how much points is gained by the player
- */
-earnpoints(earnedpoints){
-    //implements points gaining function for themed cosmetics
-    //adding the points to the cumulative total in the user profile
-    return earnedpoints;
-}
+    /**
+     * a function for player earning points recorded into the user profile
+     * @param {Number} earnedpoints - takes in a number of gained points
+     * @returns {Number} Number - returns how much points is gained by the player
+     */
+    earnpoints(earnedpoints) {
+        //implements points gaining function for themed cosmetics
+        //adding the points to the cumulative total in the user profile
+        return earnedpoints;
+    }
 
-/**
- * a function for deducting the spent points from the user profile
- * @param {Number} spendPoints - the amount of points that is going to be deducted from the user profile
- * @return {void} 
- */
-spendPoints(spendPoints){
-    //implementation of spending points for cosmetics
-    //validates if there is enough points in the user profile statistics
-    //rejects the transaction if there is not enought points
-}
+    /**
+     * a function for deducting the spent points from the user profile
+     * @param {Number} spendPoints - the amount of points that is going to be deducted from the user profile
+     * @return {void} 
+     */
+    spendPoints(spendPoints) {
+        //implementation of spending points for cosmetics
+        //validates if there is enough points in the user profile statistics
+        //rejects the transaction if there is not enought points
+    }
 
 }
