89a1b7bab59435b4117b3fb8fa1c26ead6732ff4
KevinXJarema <123217322+KevinXJarema@users.noreply.github.com>
2024-02-29 18:41:15 -0500

    Update proof_of_tests.py
    Added some more tests

--- intelligest_smart_home/tests/proof_of_tests.py
+++ intelligest_smart_home/tests/proof_of_tests.py
@@ -20,7 +20,23 @@ class TestAddNumbers(unittest.TestCase):
     
     def test_addition_positive_negative(self):
         result = add_numbers(1, -2)
-        self.assertEqual(result, -1)    
+        self.assertEqual(result, -1)
+
+    def test_addition_large_numbers(self):
+        result = add_numbers(1000000, 2000000)
+        self.assertEqual(result, 3000000)
+
+    def test_addition_decimal_numbers(self):
+        result = add_numbers(0.1, 0.2)
+        self.assertAlmostEqual(result, 0.3)
+
+    def test_addition_mixed_types(self):
+        result = add_numbers(10, 2.5)
+        self.assertAlmostEqual(result, 12.5)
+
+    def test_addition_string_input(self):
+        with self.assertRaises(TypeError):
+            add_numbers("Hello", "World")
 
 
 if __name__ == '__main__':
