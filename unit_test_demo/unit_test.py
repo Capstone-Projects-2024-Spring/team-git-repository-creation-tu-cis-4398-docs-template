65ee2a4f08366d2b49803ef5ff56d5586cb544c4
andrewto30 <andrew.to30@gmail.com>
2024-02-29 15:28:25 -0500

    Passing all test case for actions

--- unit_test_demo/unit_test.py
+++ unit_test_demo/unit_test.py
@@ -4,9 +4,6 @@ from unit import add_numbers
 class Test(unittest.TestCase):
     def test_add_numbers(self):
         self.assertEqual(add_numbers(1, 2), 3)
-    
-    def test_add_numbers_fail(self):
-        self.assertEqual(add_numbers(1, 3), 3)
 
 if __name__ == '__main__':
     unittest.main()
