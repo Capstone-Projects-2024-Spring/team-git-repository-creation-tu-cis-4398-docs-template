e2ceee335ae5f72624c446e94823ea068406b386
Brandon Zheng <bzbrandon1999@gmail.com>
2024-03-01 13:43:00 -0500

    updated comment

--- flask-backend/resource_tests/process_input_test.py
+++ flask-backend/resource_tests/process_input_test.py
@@ -1,7 +1,9 @@
 import pytest
 
 def test_post(client):
-    """Test the post method of the ProcessInput resource."""
+    """
+    Test the post method of the ProcessInput resource for successful processing of user input.
+    """
     data = {"user_input": "This is a user input"}
     response = client.post("/process_input", data=data)
     assert response.status_code == 200
