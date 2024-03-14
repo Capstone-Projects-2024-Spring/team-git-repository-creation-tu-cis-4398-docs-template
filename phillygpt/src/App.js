7ae485960573d111a0c561acf0c8e7310dbcccfb
zyij99 <bzbrandon1999@gmail.com>
2024-03-14 00:24:37 -0400

    added temp desc for phillygpt

--- phillygpt/src/App.js
+++ phillygpt/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="bg-darkgray min-h-screen">
       <Navbar />
       <div className="text-white text-center p-4 font-extrabold text-4xl"> PhillyGPT </div>
+      <div className="text-white text-center mb-4"> This is a temporary description for PhillyGPT </div>
     </div>
   );
 }
