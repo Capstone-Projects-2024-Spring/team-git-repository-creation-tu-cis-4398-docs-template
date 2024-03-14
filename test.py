ee295ca64beb50618d4c3a0937b4db182af475d9
Wenjie Gao <tul42337@temple.edu>
2024-03-13 20:55:19 -0400

    Added tests for game sessions

--- test.py
+++ test.py
@@ -64,3 +64,59 @@ class Test_User_Data():
         Result: True if a string with a row's data is returned
         """
         pass
+
+class Test_Game():
+    '''
+    Class for testing the game_session class
+    '''
+    # Client that sends requests to endpoints of the application
+    client = FlaskClient()
+
+    def test_initialization():
+        """
+        Test: A new session is initialized with players. It contains relevant information of a game
+        Result: True if the returned session is not None and have unique ID 
+        """
+        pass
+
+    def test_start():
+        """
+        Test: Ensure that the race starts successfully
+        Result: True if the race is initialized and components are set up properly
+        """
+        pass
+
+    def test_tie():
+        """
+        Test: Ensure that no winner is returned when no player has won the race
+        Result: True if the returned list is empty
+        """
+        pass
+
+    def test_winner():
+        """
+        Test: Ensure that the correct winner is returned when a single player wins the race
+        Result: True if the returned list contains the ID of the winning player only
+        """
+        pass
+
+    def test_race_end():
+         """
+        Test: Ensure that the race is ended and necessary cleanup actions are performed
+        Result: True if all cleanup actions are successfully executed
+        """
+         pass
+    
+    def test_add_player():
+        """
+        Test: Ensure that a player is added to the game session successfully
+        Result: True if the player is added and can be retrieved from the game session
+        """
+        pass
+
+    def test_remove_player():
+        """
+        Test: Ensure that a player is removed from the game session successfully
+        Result: True if the player is removed and cannot be retrieved from the game session
+        """
+        pass
