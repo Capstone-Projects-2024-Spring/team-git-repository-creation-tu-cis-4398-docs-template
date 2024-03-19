b2d8ff4c293543a588a5ca710e8daff9d21b1063
zyij99 <bzbrandon1999@gmail.com>
2024-03-19 02:07:02 -0400

    added temp light mode to searchbar

--- phillygpt/src/components/searchbar.jsx
+++ phillygpt/src/components/searchbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import CheckClass from './DarkMode/checkClass';
 
 const SearchBar = ({ userInput, handleInputChange, handleButtonClick }) => {
+
+  const isDark = CheckClass();
+  
   return (
     <div className="flex justify-center items-center mb-4">
       <input
         type="text"
         value={userInput}
         onChange={handleInputChange}
-        className="text-black rounded-l-md p-2 w-2/5 h-12"
+        className={`p-2 w-2/5 h-12 rounded-l-md ${isDark ? 'text-black bg-white' : 'text-white bg-gray-300'}`}
         placeholder="Ask me something..."
         style={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
       />
