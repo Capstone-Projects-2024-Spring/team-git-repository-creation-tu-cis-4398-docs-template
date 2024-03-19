b7c8d6067b5fcce4f576014971e7b65b5bf9460f
zyij99 <bzbrandon1999@gmail.com>
2024-03-18 21:25:04 -0400

    added home and used the rfce shortcut to set up a basic template

---
+++ phillygpt/src/pages/response.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResponsePage = () => {
+  return (
+    <div>
+      <h1>Response Page</h1>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+export default Response;
