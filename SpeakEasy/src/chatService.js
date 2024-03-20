9306f1461fb72dacacee21012a5df93c4be19d9a
Erin Santosa <tun35949@temple.edu>
2024-03-19 23:46:22 -0400

    Skeleton functions

---
+++ SpeakEasy/src/chatService.js
@@ -0,0 +1,16 @@
+/**
+ * Function to send chat message
+ * @param {string} message - User's message
+ * @returns {string} - Outputs string
+ */
+function sendChatMessage(message){
+    // Implementation here
+}
+
+/** 
+ * Function to get chat history
+ * @returns {string} - Outputs string
+ */
+function fetchChatHistory(){
+    // Implementation here
+}
