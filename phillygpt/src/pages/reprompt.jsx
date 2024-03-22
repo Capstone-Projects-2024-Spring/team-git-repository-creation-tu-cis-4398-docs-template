d9642770fa6992b826308f3b13e7573118388256
zyij99 <bzbrandon1999@gmail.com>
2024-03-22 01:50:35 -0400

    updated the reprompt page.

--- phillygpt/src/pages/reprompt.jsx
+++ phillygpt/src/pages/reprompt.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import DisplayArea from '../components/displayArea';
+import Title from '../components/title';
+import SearchBar from '../components/searchbar';
+import CheckClass from '../components/DarkMode/checkClass';
 
 const RepromptPage = () => {
+  const location = useLocation();
+  const userInput = new URLSearchParams(location.search).get('input');
+  const isDark = CheckClass();
   return (
     <div>
-      <h1>Reprompt Page</h1>
+      <p>User Input: {userInput}</p> {/*THIS IS FOR TESTING PURPOSES. REMOVE WHEN DONE BEFORE DEMO.*/}
+      <Title route = {'/reprompt'}/>
+      <SearchBar/>
+      <div className="suggestion-prompt-container">
+        <div className={`text-${isDark ? 'white' : 'black'} text-center mb-4`}> We're unable to find what you're looking for, here are some suggestions to improve your prompt. </div>
+        <DisplayArea/> {/* REPLACE WITH SUGGESTIONS*/}
+      </div>
+
     </div>
   );
 }
