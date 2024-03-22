605f44e4df0c31a1a3b868bf3179b77967ed4761
zyij99 <bzbrandon1999@gmail.com>
2024-03-22 01:24:29 -0400

    successfully transfered user input from one page to another.

--- phillygpt/src/pages/response.jsx
+++ phillygpt/src/pages/response.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ResponsePage = () => {
+  const location = useLocation();
+  const userInput = new URLSearchParams(location.search).get('input');
+
   return (
     <div>
       <h1>Response Page</h1>
-      {/* Add your content here */}
+      <p>User Input: {userInput}</p>
+
     </div>
   );
 }
