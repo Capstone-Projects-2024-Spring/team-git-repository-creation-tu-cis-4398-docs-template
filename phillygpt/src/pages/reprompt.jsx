536a2398094da300f4e52fb14f49bf404d299b42
zyij99 <bzbrandon1999@gmail.com>
2024-03-22 17:58:13 -0400

    changed font style in reprompt message.

--- phillygpt/src/pages/reprompt.jsx
+++ phillygpt/src/pages/reprompt.jsx
@@ -15,7 +15,7 @@ const RepromptPage = () => {
       <Title route = {'/reprompt'}/>
       <SearchBar/>
       <div className="suggestion-prompt-container">
-        <div className={`text-${isDark ? 'white' : 'black'} text-center mb-4`}> We're unable to find what you're looking for, here are some suggestions to improve your prompt. </div>
+        <div className={`text-${isDark ? 'white' : 'black'} text-center mb-4 font-lightbold`}> We're unable to find what you're looking for, here are some suggestions to improve your prompt. </div>
         <DisplayArea/> {/* REPLACE WITH SUGGESTIONS*/}
       </div>
 
