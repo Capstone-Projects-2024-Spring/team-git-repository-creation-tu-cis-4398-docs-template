269009a1aac5464699483eb642195899ab599cc3
gia-m12 <tul55523@temple.edu>
2024-03-28 15:52:34 -0400

    setting up character

--- platformerGame/platformer.py
+++ platformerGame/platformer.py
@@ -11,11 +11,29 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Jumpy')
 
+# define colors
+WHITE = (255, 255, 255)
+
 # load images
 bg_image = pygame.image.load('platformerGame/assets/bg.png').convert_alpha()
+jumpy_image = pygame.image.load('platformerGame/assets/jump.png').convert_alpha()
+
+# player class
+class Player():
+    def __init__(self, x, y):
+        self.image = pygame.transform.scale(jumpy_image, (45,45))
+        self.width = 25
+        self.height = 40
+        self.rect = pygame.Rect(0,0, self.width, self.height)
+        self.rect.center = (x,y)
+
+    def draw(self):
+        screen.blit(self.image, (self.rect.x - 12, self.rect.y - 5))
+        pygame.draw.rect(screen, WHITE, self.rect, 2)
 
 
 
+jumpy = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT - 150)
 
 # game loop
 run = True
@@ -23,6 +41,9 @@ while run:
 
     # draw background
     screen.blit(bg_image, (0,0))
+
+    # draw characters
+    jumpy.draw()
 
 
 
